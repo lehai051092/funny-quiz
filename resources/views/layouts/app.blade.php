5d3ae53909671dfab7e6a49cc8c8fd0900efcaf8
lehai051092 <mskumji5@gmail.com>
2019-12-11 15:01:04 +0700

    code change password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,43 @@
                             <a href="index.html"><img src="{{asset('storage/img/core-img/logo.png')}}" alt=""></a>
                         </div>
                         <div class="login-content">
-                            <a href="#">Register / Login</a>
+                            @guest
+                                <a href="{{ route('login') }}"><i class="fa fa-user"
+                                                                  aria-hidden="true" style="color: black">Login</i></a><br>
+                                @if (Route::has('register'))
+                                    <a class="nav-link"
+                                       href="{{ route('register') }}"><i class="fa fa-user-plus" style="color: black" aria-hidden="true">Register</i></a>
+                                @endif @else
+                                <ul style="list-style: none">
+                                    <li class="nav-item dropdown"><a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                                                     role="button" data-toggle="dropdown"><img
+                                                src="https://img.icons8.com/ios-filled/50/000000/user.png"><br>&nbsp;&nbsp;&nbsp;&nbsp;{{ Auth::user()->name}}
+                                        </a>
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <ul style="list-style: none">
+                                                <li>
+                                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
+                                                </li>
+                                                <li>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                          style="display: none;"> @csrf </form>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item"
+                                                       href="{{route('users.information', Auth::user()->id)}}">Profile</a>
+                                                </li>
+                                                @can('crud-users')
+                                                    <li>
+                                                        <a class="dropdown-item" href="{{route('users.list')}}">List User</a>
+                                                    </li>
+                                                @endcan
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            @endguest
                         </div>
                     </div>
                 </div>
