fa8931a496ec445b93af9eab77e12f7f1ddb37f9
lehai051092 <mskumji5@gmail.com>
2019-12-11 14:43:42 +0700

    code change password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Services\Impl\UserServiceImpl;
+use App\Rules\MatchOldPassword;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,5 +16,25 @@ class UserController extends Controller
     {
         $this->middleware('auth');
         $this->userService = $userService;
+    }
+
+    public function changePassword()
+    {
+        return view('users.changePassword');
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', new MatchOldPassword],
+            'new_password' => ['required'],
+            'new_confirm_password' => ['same:new_password'],
+        ]);
+
+        User::find(auth()->user()->id)->update(['password' => Hash::make($request->new_password)]);
+
+        $message = 'Change Password Success';
+
+        return view('users.success', compact('message'));
     }
 }
