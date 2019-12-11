7eab8e85d66a5454c038127083b09a57209cdcfd
leelaplap <letandat27101998@gmail.com>
2019-12-11 16:13:41 +0700

    upload file about and contact

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,21 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
 
 Auth::routes();
 
