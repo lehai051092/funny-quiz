4d01207c41e459bbd8cba540f30e38ee90068420
NhuTien2721996 <nhutien2721996>
2019-12-13 22:37:21 +0700

    create form list quiz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
     {{--    list users--}}
     <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
 
+
 </head>
 
 <body>
--- resources/views/quizzes/list.blade.php
+++ resources/views/quizzes/list.blade.php
@@ -5,17 +5,41 @@
             <div class="col-12 m-5">
                 <a href="{{route('quizzes.create',$category->id)}}" class="btn btn-link">Tạo Quiz</a>
                 <div class="row">
-                    @foreach($quizzes as $quiz)
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{$quiz->image}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                               <a href="{{route('quizzes.view',$quiz->id)}}"> <h5 class="card-title">{{$quiz->name}}</h5></a>
-                                <p class="card-text">{{$quiz->desc}}</p>
-                                <a href="{{route('quizzes.delete',$quiz->id)}}" class="card-link" onclick="return confirm('Are you sure delete???')">Delete</a>
-                                <a href="{{route('quizzes.edit',$quiz->id)}}" class="card-link">Edit</a>
+                    <section id="team" class="pb-5">
+                        <div class="container">
+                            <h5 class="section-title h1">List Quizzes</h5>
+                            <div class="row">
+                                <!-- Team member -->
+                                @foreach($quizzes as $quiz)
+                                <div class="col-xs-12 col-sm-4 ">
+                                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="mainflip">
+                                            <div class="frontside">
+                                                <div class="card">
+                                                    <div class="card-body text-center">
+                                                        <p><img class=" img-fluid" src="{{$quiz->image}}" alt="card image"></p>
+                                                        <h4 class="card-title">{{$quiz->name}}</h4>
+                                                        <p class="card-text">{{$quiz->desc}}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="backside">
+                                                <div class="card">
+                                                    <div class="card-body text-center mt-4">
+                                                       <a href="{{route('quizzes.view',$quiz->id)}}"> <h4 class="card-title">{{$quiz->name}}</h4></a>
+                                                        <p class="card-text">{{$quiz->desc}}</p>
+                                                        <a href="{{route('quizzes.delete',$quiz->id)}}" class="btn btn-danger" onclick="return confirm('Are you sure delete???')"><i class="fa fa-trash-alt"></i>Delete</a>
+                                                        <a href="{{route('quizzes.edit',$quiz->id)}}" class="btn btn-primary">Edit</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
                             </div>
                         </div>
-                    @endforeach
+                    </section>
                 </div>
             </div>
         </div>
