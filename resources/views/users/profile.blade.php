510b9ccbd7f56c7c747ab307b131c6aa8191ca59
leelaplap <letandat27101998@gmail.com>
2019-12-13 10:24:44 +0700

    add script in profile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -268,5 +268,17 @@
             </div>
         </div>
     </div>
+    <script>
+        $(document).on('click', '.toggle-password', function () {
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            let input = $("#password");
+            input.attr('type') === 'password' ? input.attr('type', 'text') : input.attr('type', 'password');
+        });
 
+        $(document).on('click', '.toggle-password-confirm', function () {
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            let input = $("#password-confirm");
+            input.attr('type') === 'password' ? input.attr('type', 'text') : input.attr('type', 'password');
+        });
+    </script>
 @endsection
