e7d72eb2734220c4abc45ce739ee4c924545cca1
NhuTien2721996 <nhutien2721996>
2019-12-16 08:45:19 +0700

    create form list quiz

--- resources/views/questions/list.blade.php
+++ resources/views/questions/list.blade.php
@@ -6,26 +6,6 @@
         <div class="row m-5">
             <div class="col-12 m-5">
                 <a href="{{route('questions.create',$quiz->id)}}" class="btn btn-link">Thêm mới câu hỏi</a>
-
-{{--                @foreach($questions as $question)--}}
-{{--                    <ul class="list-group">--}}
-{{--                        <li class="list-group-item active">{{$question->title}}</li>--}}
-{{--                        @foreach($answers as $answer)--}}
-{{--                            @if($answer->question_id==$question->id)--}}
-{{--                                <li class="list-group-item">{{$answer->title}}</li>--}}
-{{--                                <div>--}}
-{{--                                    <a href="{{route('answers.delete',$answer->id)}}" class="btn btn-btn-danger"--}}
-{{--                                       onclick="return confirm('Are you sure delete???')">Delete answer</a>--}}
-{{--                                    <a href="{{route('answers.edit',$answer->id)}}">Edit answer</a>--}}
-{{--                                </div>--}}
-{{--                            @endif--}}
-{{--                        @endforeach--}}
-{{--                    </ul>--}}
-{{--                    <a href="{{route('answers.create',$question->id)}}">Thêm câu trả lời</a><br>--}}
-{{--                    <a href="{{route('questions.delete',$question->id)}}" class="btn btn-danger"--}}
-{{--                       onclick="return confirm('Are you sure delete???')">Delete question</a>--}}
-{{--                    <a href="{{route('questions.edit',$question->id)}}" class="btn btn-primary">Edit question</a>--}}
-{{--                @endforeach--}}
                 @foreach($questions as $key=>$question)
                     <div class="container-fluid" style="background-color: #2fa360">
                     <div class="modal-dialog">
