074c172d182c811a26f2dfd4249a6a7ab5774781
leelaplap <letandat27101998@gmail.com>
2019-12-16 10:47:22 +0700

    oki

--- resources/views/create-quiz/basic-info.blade.php
+++ resources/views/create-quiz/basic-info.blade.php
@@ -5,7 +5,7 @@
         <div class="container pt-5 d-flex justify-content-center ">
             <div class="col-10">
                 <div class="row">
-                    <div class="card m-5" style="width: 100%;background-color: inherit">
+                    <div class="card m-5" style="width: 100%">
                         <div class="card-header" style="background: linear-gradient(coral,pink,yellow)">
                             <h1 class="text-center" style="color: brown">Create Quiz - Basic Information</h1>
                         </div>
@@ -13,7 +13,7 @@
                             <form>
                                 <div class="form-group">
                                     <label><h5>Title</h5></label>
-                                    <input type="text" class="form-control"style="background-color: inherit; color: white">
+                                    <input type="text" class="form-control">
                                 </div>
                                  <div class="form-group">
                                     <label><h5>Desc</h5></label>
@@ -32,8 +32,11 @@
                                 <div class="form-group">
                                     <img src="" id="imgProfile"
                                          style="width: 150px; height: 150px" class="img-thumbnail"/>
-                                    <br><br>
-                                    <input type="file" >
+                                    <div class="middle">
+                                        <input type="button" class="btn btn-secondary" id="btnChangePicture"
+                                               value="Change"/>
+                                        <input type="file" style="display: none;" id="profilePicture" name="image"/>
+                                    </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
