e1e82644cec87bf586e632e1259788d62d6f9bad
leelaplap <letandat27101998@gmail.com>
2019-12-19 16:01:27 +0700

    form create

--- resources/views/questions/createForm.blade.php
+++ resources/views/questions/createForm.blade.php
@@ -71,6 +71,22 @@
                             </div>
                         </div>
 
+                        <div class="form-group">
+                            <div class="col-12">
+                                <div class="row">
+                                    <label class="col-2"><h4><i class="fa">Answer</i></h4></label>
+                                    <div class="col-9">
+                                        <input type="text" class="form-control" name="">
+                                    </div>
+                                    <div class="col-1">
+                                        <a href=""><i class="fa fa-check" aria-hidden="true"></i></a>&nbsp;
+                                        <a href=""><i class="fa fa-trash" style="color: red" aria-hidden="true"></i></a>
+                                    </div>
+                                    <a href=""><h5><i class="fa fa-plus" aria-hidden="true"> Answer one more</i></h5></a>
+
+                                </div>
+                            </div>
+                        </div>
                         <div class="pl-3">
                             <button type="submit" class="btn btn-success text-center ">Create</button>
                         </div>
