b118ef251fd5e2e47021dd34f1822d8c9f3664c2
leelaplap <letandat27101998@gmail.com>
2019-12-26 08:59:11 +0700

    push result

--- resources/views/questions/list.blade.php
+++ resources/views/questions/list.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts.app')
 @section('content')
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <style>
+        input[type=submit]{
+            margin-left: 0.5em;
+            height: 2.5em;
+            padding: 0.2em 1em 0.2em 2.25em;
+            font-size: 2em;
+            font-weight: bold;
+            font-family: "Open Sans";
+            text-transform: uppercase;
+            color: #696666;
+            background: url({{asset('storage/img/quiz/Th606mh.png')}}) no-repeat scroll 0.1em 0.07em transparent;
+            background-size: 54px 150px;
+            border-radius: 2em;
+            border: 0.15em solid #F9C23C;
+            cursor: pointer;
+            transition: all 0.3s ease 0s;
+        }
+        input[type="submit"]:hover {
+            color: #fff;
+            background-color: #EAA502;
+            border-color: #EAA502;
+            background-position: 0.75em bottom;
+            -webkit-transition: all 0.3s ease;
+            -ms-transition: all 0.3s ease;
+            transition: all 0.3s ease;
+        }
+        input[type="submit"]:focus {
+            background-position: 2em -4em;
+            -webkit-transition: all 0.3s ease;
+            -ms-transition: all 0.3s ease;
+            transition: all 0.3s ease;
+        }
+        /* Webfonts */
+
+        @font-face {
+            font-family: 'Open Sans';
+            font-style: normal;
+            font-weight: 700;
+            src: local('Open Sans Bold'), local('OpenSans-Bold'), url(https://themes.googleusercontent.com/static/fonts/opensans/v8/k3k702ZOKiLJc3WVjuplzHhCUOGz7vYGh680lGh-uXM.woff) format('woff');
+        }
+    </style>
 <div class=" p-5">
     <form method="post" action="{{route('session.result',$quiz->id)}}">
         @csrf
@@ -21,7 +62,7 @@
                                             <label class="element-animation1 btn btn-lg btn-success btn-block"><span
                                                     class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span>
                                                 <input
-                                                    type="checkbox" name="answer[]"
+                                                    type="checkbox" name="answer[]" class="myCheckResult"
                                                     value="{{$answer->status}},{{$answer->title}}">{{$answer->title}}</label><br>
                                         </div>
                                     @endif
@@ -34,7 +75,9 @@
                 </div>
             @endforeach
                 <div style="text-align: center">
-                    <input type="submit" value="submit" class="btn btn-primary">
+                    <form>
+                        <input id="send" value="Send" type="submit"  style="display: none"/>
+                    </form>
                 </div>
         </div>
     </form>
